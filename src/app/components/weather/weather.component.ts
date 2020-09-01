96743c7293f3fc10d1a3d4b0f86efc9ae04b5706
Mustafa Yurdakul <yurdakulmustafa@outlook.com>
2020-09-01 16:22:47 +0300

    Weather component updated & localization added.

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { FormBuilder, FormGroup } from '@angular/forms'
 import { formatDate } from '@angular/common'
-import { TranslateService } from '@ngx-translate/core'
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core'
 import { WeatherapiService } from '../../services/weatherAPIService/weatherapi.service'
 
 @Component({
@@ -14,6 +14,7 @@ export class WeatherComponent implements OnInit {
     public weatherSearchForm: FormGroup
     public todayData: any
     public forecastData: any
+    public lang
 
     public lat: any
     public lon: any
@@ -23,10 +24,11 @@ export class WeatherComponent implements OnInit {
 
     constructor(private formBuilder: FormBuilder,
         private weatherapi: WeatherapiService,
-        translate: TranslateService) {
+        private translate: TranslateService) {
 
         translate.setDefaultLang('en');
         translate.use('en');
+
         this.todaysDataTime = formatDate(this.today, 'hh:mm a', 'en-US')
     }
 
@@ -34,24 +36,33 @@ export class WeatherComponent implements OnInit {
         this.weatherSearchForm = this.formBuilder.group({
             location: ['']
         })
+
+        this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+            this.lang = event.lang
+        });
     }
 
-    openWeatherRequest(form) {
-        this.weatherapi.getOpenWeather(form.location).subscribe(response => {
+    openWeatherRequest(form, lang = this.lang) {
+        this.weatherapi.getOpenWeather(form.location, lang).subscribe(response => {
             this.todayData = response
             console.log(response)
 
             var coordinates = Object.values(response)[0]
             this.lat = coordinates.lat
             this.lon = coordinates.lon
-            this.openForecastRequest(this.lat, this.lon)
+            this.openForecastRequest(this.lat, this.lon, lang)
         })
     }
 
-    openForecastRequest(lat, lon) {
-        this.weatherapi.getOpenForecast(lat, lon).subscribe(response => {
+    openForecastRequest(lat, lon, lang = this.lang) {
+        this.weatherapi.getOpenForecast(lat, lon, lang).subscribe(response => {
             this.forecastData = response
             console.log(response)
         })
     }
+
+    switchLanguage(language: string) {
+        this.translate.use(language);
+    }
+
 }
