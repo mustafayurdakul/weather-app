d08609425fb427417c7c80be491166955bfad9ec
Mustafa Yurdakul <yurdakulmustafa@outlook.com>
2020-09-01 14:40:20 +0300

    Icons added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+/* Angular */
+import { NgModule } from '@angular/core'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule, HttpClient } from '@angular/common/http'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { AboutComponent } from './components/about/about.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+/* Routing  */
+import { AppRoutingModule } from './app-routing.module'
 
-import { MatMenuModule } from '@angular/material/menu/';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { WeatherComponent } from './components/weather/weather.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { MatInputModule } from '@angular/material/input';
+/* Components */
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './components/header/header.component'
+import { AboutComponent } from './components/about/about.component'
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+/* Angular Material */
+import { MatMenuModule } from '@angular/material/menu/'
+import { MatButtonModule, } from '@angular/material/button'
+import { MatButtonToggleModule } from '@angular/material/button-toggle'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
+import { WeatherComponent } from './components/weather/weather.component'
+import { ContactComponent } from './components/contact/contact.component'
+import { MatInputModule } from '@angular/material/input'
+import { MatDividerModule } from '@angular/material/divider';
 
-import { HttpClientModule } from '@angular/common/http';
+/* Platform Browser */
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
+
+/* ngx - Translate */
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
 
 @NgModule({
     declarations: [
@@ -35,12 +50,22 @@ import { HttpClientModule } from '@angular/common/http';
         BrowserAnimationsModule,
         MatMenuModule,
         MatButtonModule,
+        MatButtonToggleModule,
         MatToolbarModule,
         MatCardModule,
         MatIconModule,
+        MatDividerModule,
         MatInputModule,
         FormsModule,
-        ReactiveFormsModule, HttpClientModule
+        ReactiveFormsModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })
     ],
     providers: [],
     bootstrap: [AppComponent]
