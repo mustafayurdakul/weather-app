8208d16eaa6a0afbefe3415d8274105264107752
Mustafa Yurdakul <yurdakulmustafa@outlook.com>
2020-09-01 14:40:00 +0300

    Adding localization to header component, SCSS updated.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    constructor(private translate: TranslateService) {
+        translate.setDefaultLang('en');
+        translate.use('en');
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    switchLanguage(language: string) {
+        this.translate.use(language);
+    }
 }
