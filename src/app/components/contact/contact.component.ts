15b2341c9fa800fc1ce3614080532d651aaf91ed
Mustafa Yurdakul <yurdakulmustafa@outlook.com>
2020-09-02 11:18:11 +0300

    WebSocket added

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { webSocket } from "rxjs/webSocket";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+    selector: 'app-contact',
+    templateUrl: './contact.component.html',
+    styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+    message = "hello"
+    subject = webSocket("");
 
-  constructor() { }
+    constructor() {
 
-  ngOnInit(): void {
-  }
+    }
+
+    ngOnInit(): void {
+
+    }
+
+    sendToServer($event) {
+        this.subject.subscribe(
+            msg => console.log('message received: ' + msg), // Called whenever there is a message from the server.
+            err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+            () => console.log('complete') // Called when connection is closed (for whatever reason).
+        )
+        this.subject.next(this.message)
+        this.subject.complete()
+    }
 
 }
